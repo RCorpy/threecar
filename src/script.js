111577eb7ae506a11c66346b4f8c21e67f8cc662
Ramón Navarro <rnavarro@maubic.es>
2021-04-16 19:05:22 +0200

    first commit

--- src/script.js
+++ src/script.js
@@ -9,32 +9,26 @@ const gui = new dat.GUI()
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-// Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const playerCar = Car()
+scene.add(playerCar)
 
-// Materials
+// LIGHTS
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6)
+scene.add(ambientLight)
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+//add directional light
 
-// Lights
+const dirLight = new THREE.DirectionalLight(0xffffff,0.6)
+dirLight.position.set(100, -300, 400)
+scene.add(dirLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
 
-/**
- * Sizes
- */
+
+//RESIZER
+
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -55,51 +49,75 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+
+// CAMERA
+
+const aspectRatio = window.innerWidth/window.innerHeight
+const cameraWidth = 150
+const cameraHeight = cameraWidth / aspectRatio
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
 camera.position.x = 0
 camera.position.y = 0
 camera.position.z = 2
-scene.add(camera)
 
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+camera.position.set(200,-200,300)
+camera.up.set(0,0,1)
+camera.lookAt(0,0,0)
 
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
+// RENDERER
 
-const clock = new THREE.Clock()
+const renderer = new THREE.WebGL1Renderer({antialias: true})
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.render(scene, camera)
 
-const tick = () =>
-{
+document.body.appendChild(renderer.domElement)
 
-    const elapsedTime = clock.getElapsedTime()
+// CAR
 
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+function Car(){
+    const car = new THREE.Group()
+    
+    const backwheel = new THREE.Mesh(
+        new THREE.BoxBufferGeometry(12,33,12),
+        new THREE.MeshLambertMaterial({color: 0x333333})
+    )
 
-    // Update Orbital Controls
-    // controls.update()
+    backwheel.position.z = 6
+    backwheel.position.x = -18
 
-    // Render
-    renderer.render(scene, camera)
+    car.add(backwheel)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+    const frontwheel = new THREE.Mesh(
+        new THREE.BoxBufferGeometry(12,33,12),
+        new THREE.MeshLambertMaterial({color: 0x333333})
+    )
+
+    frontwheel.position.z = 6
+    frontwheel.position.x = 18
+
+    car.add(frontwheel)
+
+    const main = new THREE.Mesh(
+        new THREE.BoxBufferGeometry(60,30,15),
+        new THREE.MeshLambertMaterial({color: 0xa52523})
+    )
+
+    main.position.z=12
+
+    car.add(main)
+
+    const cabin = new THREE.Mesh(
+        new THREE.BoxBufferGeometry(33,24,12),
+        new THREE.MeshLambertMaterial({color: 0xffffff})
+    )
+
+    cabin.position.x = -6
+    cabin.position.z = 25.5
+
+    car.add(cabin)
+
+    return car
+
 }
-
-tick()
